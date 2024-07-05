d525e91b9b71e231b3625975f5825ac114e8f61a
savariammal <160878030+savariammal@users.noreply.github.com>
2024-07-05 18:33:21 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -1,11 +1,14 @@
 let outputscreen = document.getElementById("output");
 let equalsButton = document.getElementById("equals");
 
-function display(num) {
+function display(num) 
+ {
     // Only allow numbers, operators, and decimal point
     const validInputs = "0123456789+-*/.%";
-    if (validInputs.includes(num)) {
-        if (outputscreen.value === "undefined") {
+    if (validInputs.includes(num)) 
+        {
+        if (outputscreen.value === "undefined") 
+        {
             outputscreen.value = "";
         }
         outputscreen.value += num;
@@ -13,13 +16,17 @@ function display(num) {
         equalsButton.disabled = false;
     }
 }
-
-function Calculate() {
+function Calculate() 
+{
     try {
-        // If outputscreen is empty, set it to "0"
-        if (outputscreen.value === "") {
+        // If outputscreen is empty or contains invalid sequence set it to "0"
+        
+      if (outputscreen.value === "" || /[^\d]+$/.test(outputscreen.value) || /^[-+/*%.]+$/.test(outputscreen.value)) 
+        {
             outputscreen.value = "0";
-        } else {
+        } 
+        else 
+        {
             // Remove the last character if it is an operator
             const lastChar = outputscreen.value.slice(-1);
             const operators = "+-*/.%";
@@ -30,18 +37,21 @@ function Calculate() {
         }
         // Disable the equals button after calculation
         equalsButton.disabled = true;
-    } catch (err) {
+    } 
+    catch (err)
+     {
         alert("Enter valid inputs");
         Clear();
     }
 }
-
-function Clear() {
+function Clear()
+ {
     outputscreen.value = "";
     equalsButton.disabled = false;
 }
 
-function del() {
+function del() 
+{
     outputscreen.value = outputscreen.value.slice(0, -1);
     // Enable the equals button if it was disabled
     equalsButton.disabled = false;
